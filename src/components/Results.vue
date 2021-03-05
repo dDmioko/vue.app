6686b04db931b7ac9224f7844bc222b56e5d8210
Aleksey (dDmioko) Anisimov <aleksejj.anisimov@gmail.com>
2021-03-05 12:59:33 +0700

    Fix formating

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -10,12 +10,7 @@ export default {
 
 <style>
 .result {
-    /* width: 400px;
-    border-radius: 20px;
-    background: #0faf87; */
     color: #0faf87;
     text-align: center;
-    /* padding: 100px 0;
-    margin: 40px auto; */
 }
 </style>
